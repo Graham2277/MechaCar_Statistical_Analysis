beebe155d9a9467797fca0bc7877b4b2c861e67f
Graham Caplan <you@example.com>
2022-01-21 16:26:41 -0800

    Adding files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,17 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =MechaCar_mpg)) 
+
+#Deliverable 2: Create Visualizations for the Trip Analysis
+head(Suspension_Coil)
+
+Mean = mean(Suspension_Coil$PSI)
+Median=median(Suspension_Coil$PSI)
+Variance=var(Suspension_Coil$PSI)
+SD = sd(Suspension_Coil$PSI)
+
+#Total_Summary Data Frame
+total_Summary <- data.frame(Mean,Median,Variance,SD)
+
+#summary by lot
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
