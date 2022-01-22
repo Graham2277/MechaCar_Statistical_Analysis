0e85416746685a3c0ae68ed0af5cdbaec9c864f8
Graham Caplan <you@example.com>
2022-01-22 10:28:01 -0800

    adding files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,11 @@ total_Summary <- data.frame(Mean,Median,Variance,SD)
 
 #summary by lot
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
+
+#Deliverable 3: T-Tests on Suspension Coils
+t.test((Suspension_Coil$PSI),mu = 1500)
+
+# t-test for each lot
+t.test(subset(Suspension_Coil,Manufacturing_Lot =="Lot1")$PSI,mu = 1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot =="Lot2")$PSI,mu = 1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot =="Lot3")$PSI,mu = 1500)
